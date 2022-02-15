a023ac231ee0f4728601720c4af78c02264b4c3b
M.Sitthibudhi <sitthiput.m@gmail.com>
2022-02-15 12:02:12 +0700

    mock-up code for guidline

--- app/schemas.py
+++ app/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 # Example of Schemas for req-res:
 
-# class Fuzz_input(BaseModel):
-#     url: str
-#     class Config:
-#         orm_mode = True
+class Test_input(BaseModel):
+    message: str
+    class Config:
+        orm_mode = True
