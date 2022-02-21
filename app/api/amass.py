74bf22e980681b2f6d0e4aea52e130cf81cbdb34
KoFeeBriAN <coffeetk2017@gmail.com>
2022-02-21 12:25:06 +0700

    feat: basic amass db services
    - get enumerations by domain
    - get latest enumeration
    
    todo
    - proper handling exceptions and errors

--- app/api/amass.py
+++ app/api/amass.py
@@ -5,16 +5,28 @@ import logging, os, json
 from app.service.amass.db import DBClient
 
 
-# client = DBClient()
+client = DBClient()
 
 router = APIRouter(prefix="/db")
 
-@router.post("/latest")
+@router.get("/")
+def get_enum_by_domain(domain: str):
+  try:
+    result = client.get_enum(domain=domain)
+    return JSONResponse(status_code=200, content=result)
+  except Exception as e:  
+    print(e)
+    return JSONResponse(status_code=400, content={
+      "message": "error"
+    })
+
+@router.get("/latest")
 def get_latest_enum() -> JSONResponse:
-  return JSONResponse(
-    status_code=200,
-    content = {
-      "Hello": "World"
-    }
-  )
-  # client.get_latest_enum()
+  try:
+    result = client.get_enum(config={ "latest": True })
+    return JSONResponse(status_code=200, content=result)
+  except Exception as e:  
+    print(e)
+    return JSONResponse(status_code=400, content={
+      "message": "error"
+    })
--- app/service/amass/db.py
+++ app/service/amass/db.py
@@ -1,7 +1,9 @@
 import grpc
+from google.protobuf.json_format import MessageToDict
 from app.protos.amass.db import db_service_pb2_grpc as pb2_grpc
 from app.protos.amass.db import db_message_pb2 as pb2_msg
 
+import logging
 class DBClient(object):
     """
     Client from Amass DB Service
@@ -9,20 +11,22 @@ class DBClient(object):
 
     def __init__(self):
         self.host = "localhost"
-        self.server_port = "50051"
+        self.server_port = "3000"
 
         self.channel = grpc.insecure_channel(f"{self.host}:{self.server_port}")
 
         self.stub = pb2_grpc.DBServiceStub(self.channel)
-
-    def get_latest_enum(self):
+    
+    def get_enum(self, domain: str = "", config: dict = None) -> pb2_msg.DBResult:
         """
-        Get the latest enumeration result
-        ex. Use after run enumeration
+        Get enum
         """
-
-        # TODO: use proper id
-        request = pb2_msg.DBRequest( id="user-1", config={ "latest": True })
-
-        response = self.stub.Run(request=request)
-        print(response)
+        assert(len(domain) != 0 or (len(domain) == 0 and config != None))
+        
+        try:
+            request = pb2_msg.DBRequest(id="user-1", domain=domain, config=config)
+            response = self.stub.Run(request=request)
+            return MessageToDict(response.result)
+        except grpc.RpcError as e:
+            logging.error(e)
+            raise
