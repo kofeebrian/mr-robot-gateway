e4ba0e0d6b06856a0045f3f0459107bb9e746f66
Sitthiput <sitthiput.m@gmail.com>
2022-03-06 20:36:41 +0700

    create client for FFUF

---
+++ app/service/ffuf/ffuf.py
@@ -0,0 +1,32 @@
+import logging
+from flask import request
+from starlette.responses import JSONResponse
+from fastapi import Response
+import requests
+
+class FFUFClient(object):
+    """
+    Client for FFUF Service
+    """
+
+    def __init__(self):
+        self.server = "localhost:8000/api/scan"
+
+    def fuzzing(self, url: str) -> Response:
+        try:
+            if len(url) > 0:
+                return JSONResponse(
+                    status_code = 200,
+                    content = requests.post(self.server, data=url)
+                )
+            else:
+                return JSONResponse(
+                    status_code = 400,
+                    content = {"message": "Invalid Request"}
+                )
+        except Exception as e:
+            logging.error(f"Enumeration get failed with {e}")
+            return JSONResponse(
+                status_code=400,
+                content={"message": "error"}
+            )
