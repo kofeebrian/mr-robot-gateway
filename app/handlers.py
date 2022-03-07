db15e3421a6ee4ae3dd40afaaefe20b922602fbc
Sitthiput <sitthiput.m@gmail.com>
2022-03-07 18:10:50 +0700

    handle only router for services

--- app/handlers.py
+++ app/handlers.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
 
-from .api import amass, test
+from .api import service
 
 app = FastAPI()
 
-# Example for creating API's router
-
 app.include_router(
-    test.router,
-    prefix="/api",
-    responses={404: {"message": "Not Found"}},
-)
-
-app.include_router(
-    amass.router,
-    prefix="/api/amass",
+    service.router,
+    prefix="/api/service",
     responses={404: {"message": "Not Found"}},
 )
