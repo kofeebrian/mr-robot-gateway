d2865ecf490d7a8f7e21156fa8e74d29abf82b81
Sitthiput <sitthiput.m@gmail.com>
2022-03-07 18:11:12 +0700

    init WAPP client

---
+++ app/service/wapp/wapp.py
@@ -0,0 +1,32 @@
+import logging
+from flask import request
+from starlette.responses import JSONResponse
+from fastapi import Response
+import requests
+
+class WAPPClient(object):
+    """
+    Client for Wappalyzer Service
+    """
+
+    def __init__(self):
+        self.server = "localhost:8000/xxx"
+
+    def scanning(self, url: str) -> Response:
+        try:
+            if len(url) > 0:
+                return JSONResponse(
+                    status_code = 200,
+                    content = requests.post(self.server, data=url)
+                )
+            else:
+                return JSONResponse(
+                    status_code = 400,
+                    content = {"message": "Invalid Request"}
+                )
+        except Exception as e:
+            logging.error(e)
+            return JSONResponse(
+                status_code=400,
+                content={"message": "error"}
+            )
